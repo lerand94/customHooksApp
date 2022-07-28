7702b2591b8b412b9fef48c4f27702b25be163ab
Kirill <27140985+lerand94@users.noreply.github.com>
2022-07-28 18:56:28 +0300

    finnaly compled custom hook for get and post request

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,31 +1,36 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
-const useHttp = (requestConfig) => {
+const useHttp = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const sendRequest = async (taskText, applyData) => {
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
     setIsLoading(true);
     setError(null);
     try {
       const response = await fetch(requestConfig.url, {
-        method: requestConfig.method,
-        headers: requestConfig.headers,
-        body: JSON.stringify(requestConfig.body),
+        method: requestConfig.method ? requestConfig.method : "GET",
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
       });
 
       if (!response.ok) {
         throw new Error("Request failed!");
       }
 
+      const data = await response.json();
       applyData(data);
     } catch (err) {
       setError(err.message || "Something went wrong!");
     }
     setIsLoading(false);
-  };
+  }, []);
 
-  return { isLoading: isLoading, error: error, sendRequest: sendRequest };
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
 };
 
 export default useHttp;
